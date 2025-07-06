0ba5fdb230311edb8c8f9672585a2d3c4e058426
Mario Rodríguez <mariocrg3@gmail.com>
2025-07-06 21:54:05 +0000

    added pyplot
    Co-authored-by: Andres2806 <andressaballos20@gmail.com>

--- Main.py
+++ Main.py
@@ -6,3 +6,7 @@ matrix = [[i**2] for i in range(5)]
 ref = matrix
 matrixCopy = copy.deepcopy(matrix)
 print(f"matrix {matrix} matrix ref {ref} matrix copy {matrixCopy}")
+
+plt.figure()
+plt.plot(matrixCopy[0][0],matrixcopy[0][1])
+plt.show()
