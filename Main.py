ff756acd7529b3c2061264858f81dff4446e566b
Mario Rodríguez <mariocrg3@gmail.com>
2025-07-06 15:58:20 -0600

    added more info (#2)
    * added readme file
    
    * added pyplot
    
    Co-authored-by: Andres2806 <andressaballos20@gmail.com>
    
    ---------
    
    Co-authored-by: Andres2806 <andressaballos20@gmail.com>

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 # testing python file
 import copy
+import matplotlib.pyplot as plt
+
 matrix = [[i**2] for i in range(5)]
 ref = matrix
 matrixCopy = copy.deepcopy(matrix)
 print(f"matrix {matrix} matrix ref {ref} matrix copy {matrixCopy}")
+
+plt.figure()
+plt.plot(matrixCopy[0][0],matrixcopy[0][1])
+plt.show()
